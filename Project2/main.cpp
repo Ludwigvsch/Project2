2606cf360358b072580861cd94e8970de43a93a8
Ludwigvsch <lvschoenfeldt@gmx.de>
2022-04-07 17:12:20 -0700

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Ludwig von Schönfeldt  on 07.04.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
